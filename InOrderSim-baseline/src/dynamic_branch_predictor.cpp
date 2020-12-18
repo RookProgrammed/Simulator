81dcd32c661656db6c6eeb78816968d2a138f717
RookProgrammed <76138785+RookProgrammed@users.noreply.github.com>
2020-12-18 08:15:02 -0500

    tweaks in code--try once in ur execution or else will restore

--- InOrderSim-baseline/src/dynamic_branch_predictor.cpp
+++ InOrderSim-baseline/src/dynamic_branch_predictor.cpp
@@ -1,82 +1,77 @@
+/*MIPS pipeline cycle-accurate simulator*/
 
 #include "dynamic_branch_predictor.h"
 #include <iostream>
-#include <cstdio> 
+
 #include <math.h>
+#include <cstdio>
 
-DynamicNTBranchPredictor::DynamicNTBranchPredictor(uint32_t historyTableAndBTBSize, int bhtWidth, int phtWidth, int rasSize) { // MAYBE, DOES NOT REQUIRE TWO SIZES AS BOTH SIZES MUST BE SAME.
+---check
+BranchPredict_NT::BranchPredict_NT(uint32_t histTab_BTBSize, int Width_bht, int Width_pht, int rasSize) { // Not sure about this as both might have same sizes
     
-    takenCharacter = 1;
-    nottakenCharacter = 0;
-    RASSize = rasSize;
-    tagMask = (historyTableAndBTBSize - 1)<<2;
-    BHTWidth = bhtWidth;
-    PHTWidth = phtWidth;
+    notTake_Character = 0;take_Character = 1;
+    RASSize = rasSize; notTake_Character = 0;
+    MaskTag = (histTab_BTBSize - 1)<<2;
     
-    numberOfOutcomes = pow(2, PHTWidth);
-    for (int i = 0; i<numberOfOutcomes; i++)
+    Width_PHT = Width_pht; Width_BHT = Width_bht;
+    
+    OutcomeCount = pow(2, Width_PHT);
+    for (int i = 0; i<OutcomeCount; i++)
     {
-            outcomeToBool[i] = true;
-    }
-    int numberPHTEntries = pow(2, BHTWidth);
-    
-std::vector<int> pattern (BHTWidth);
-
-    initPHT(BHTWidth, pattern, 0);
-
-    for (int i=0; i < historyTableAndBTBSize; i++)
-
-    {
-        isBranchAndHistoryRow row;std::vector<int> pattern;
-
-        for (int j =0; j<numberPHTEntries; j++)
+        if(i<=(OutcomeCount/2)-1)
         {
-            pattern.push_back(0);
+            outcome_Bool[i] = false;
+        }else{
+            outcome_Bool[i] = true;
         }
-        row.branchPatterHistory = pattern;
-
-        row.targetAddress = 0;
-
-        globalHistoryAndBranchTargetTable[i] = row;
     }
-
-    mostRecentlyUsedAddrTag = (uint32_t)0;
-
+    int num_PHTEntries = pow(2, Width_BHT);std::vector<int> pattern (Width_BHT);
+    initial_PHT(Width_BHT, pattern, 0);
+    for (int m=0; m < histTab_BTBSize; m++)
+    {
+        BranchHistory row;
+        std::vector<int> pattern;
+        for (int n =0; n<num_PHTEntries; n++)
+        {
+          pattern.push_back(0);
+        }
+       row.Addres_Target = 0; row.branchPatternHist = pattern;globalHist_BranchTargetTab[m] = row;
+        
+    }
+    AddrTag_mostRecent = (uint32_t)0;
 }
 
-void DynamicNTBranchPredictor::initPHT(int numberPHTEntries, std::vector<int> pattern, int pos)
+void BranchPredict_NT::initial_PHT(int num_PHTEntries, std::vector<int> pattern, int pos)
 {
-    if(pos == numberPHTEntries)
-    
-{
-
-        patternHistoryTable[pattern] = 0;
-
+    if(pos == num_PHTEntries)
+    {
+        patternHist_Tab[pattern] = 0;
         return;
-
     }
-    pattern[pos] = 0;//false; 
+    pattern[pos] = 0;//false;
+    initial_PHT(num_PHTEntries, pattern, pos+1);
     
     pattern[pos] = 1;//true;
-    
-initPHT(numberPHTEntries, pattern, pos+1);    return;
+    initial_PHT(num_PHTEntries, pattern, pos+1);
+    return;
 }
 
-DynamicNTBranchPredictor::~DynamicNTBranchPredictor() {
-    // Auto-gene destruct
+BranchPredict_NT::~BranchPredict_NT() {
+   
 }
 
-uint32_t DynamicNTBranchPredictor::getTarget(uint32_t PC, int opCode, int subopCode) {
+uint32_t BranchPredict_NT::getTarget(uint32_t PC, int op_Code, int subop_Code) {
     uint32_t addrTag = getTag(PC);
-    if (checkIfInstructionIsReturn(opCode, subopCode))
+
+    if (InstructReturn_check(op_Code, subop_Code))
     {
         
-        uint32_t targetAddress = RAS.front();
-        std::cout<<"targetAddress RAS:" << targetAddress << '\n';
+        uint32_t Addres_Target = RAS.front();
+        std::cout<<"Addres_Target RAS:" << Addres_Target << '\n';
         RAS.pop_front();
-        return targetAddress;
+        return Addres_Target;
     }
-    else if(checkIfInstrunctionIsJump(opCode))
+    else if(JumpInstrunction_check(op_Code))
     {
         std::cout<<"JUMP instruction \n";
         if(RAS.size() == RASSize)
@@ -87,20 +82,20 @@ uint32_t DynamicNTBranchPredictor::getTarget(uint32_t PC, int opCode, int subopC
         RAS.push_front(PC+4);
     }
     
-    if ( checkIfPCNotABranch(opCode, subopCode)  || doesPCNotExistsInBTB_GHT(addrTag) )
+    if ( PCcheck_NotBranch(op_Code, subop_Code)  || PCcheckInBTB_GHT(addrTag) )
     {
         std::cout << "IN if branch \n";
         return -1;
     }else{
         int x;
-        uint32_t target = checkPredictorAndGetTarget(addrTag);
+        uint32_t target = PredictCheck_GetTarget(addrTag);
         return target;
     }
 }
 
-bool DynamicNTBranchPredictor::checkIfInstructionIsReturn(int opCode, int subopCode)
+bool BranchPredict_NT::InstructReturn_check(int op_Code, int subop_Code)
 {
-    if (opCode == OP_SPECIAL && (subopCode == SUBOP_JALR || subopCode == SUBOP_JR))
+    if (op_Code == OP_SPECIAL && (subop_Code == SUBOP_JALR || subop_Code == SUBOP_JR))
     {
         return true;
     }
@@ -108,9 +103,9 @@ bool DynamicNTBranchPredictor::checkIfInstructionIsReturn(int opCode, int subopC
 }
 
 
-bool DynamicNTBranchPredictor::checkIfInstrunctionIsJump(int opCode)
+bool BranchPredict_NT::JumpInstrunction_check(int op_Code)
 {
-    if(opCode == OP_JAL || opCode == OP_J)
+    if(op_Code == OP_JAL || op_Code == OP_J)
     {
         return true;
     }
@@ -118,15 +113,15 @@ bool DynamicNTBranchPredictor::checkIfInstrunctionIsJump(int opCode)
         
 }
 
-bool DynamicNTBranchPredictor::checkIfPCNotABranch(int opCode, int subopCode)
+bool BranchPredict_NT::PCcheck_NotBranch(int op_Code, int subop_Code)
 {
-    if (opCode == OP_BRSPEC) {
-        if(subopCode == BROP_BLTZ || subopCode== BROP_BGEZ || subopCode == BROP_BLTZAL || subopCode == BROP_BGEZAL )
+    if (op_Code == OP_BRSPEC) {
+        if(subop_Code == BROP_BLTZ || subop_Code== BROP_BGEZ || subop_Code == BROP_BLTZAL || subop_Code == BROP_BGEZAL )
         {
             return false;
         }
     }
-    else if(opCode == OP_BEQ || opCode== OP_BNE || opCode == OP_BLEZ || opCode == OP_BGTZ )
+    else if(op_Code == OP_BEQ || op_Code== OP_BNE || op_Code == OP_BLEZ || op_Code == OP_BGTZ )
     {
         return false;
 
@@ -134,27 +129,28 @@ bool DynamicNTBranchPredictor::checkIfPCNotABranch(int opCode, int subopCode)
     return true;
 }
 
-uint32_t DynamicNTBranchPredictor::getTag(uint32_t PC)
+uint32_t BranchPredict_NT::getTag(uint32_t PC)
 {
-        return (PC & tagMask)>>2;
+
+  return (PC & MaskTag)>>2;
+}
+bool BranchPredict_NT::PCcheckInBTB_GHT(uint32_t addrTag)
+{
+ return globalHist_BranchTargetTab.find(addrTag) == globalHist_BranchTargetTab.end() ? true: false;
 }
 
-bool DynamicNTBranchPredictor::doesPCNotExistsInBTB_GHT(uint32_t addrTag)
-{
-    return globalHistoryAndBranchTargetTable.find(addrTag) == globalHistoryAndBranchTargetTable.end() ? true: false;
-}
+uint32_t BranchPredict_NT::PredictCheck_GetTarget(uint32_t addrTag)
+{ std::vector<int>patternHistory = PatternHistory_fetch(addrTag);
+    int lookupTab = Branch_Outcome(patternHistory);
 
-uint32_t DynamicNTBranchPredictor::checkPredictorAndGetTarget(uint32_t addrTag)
-{
-    std::vector<int>patternHistory = getPatternHistory(addrTag);
-    int branchOutcome = getBranchOutcome(patternHistory);
     for (int i =0; i< patternHistory.size(); i++)
     {
         std::cout << patternHistory[i] << ' ';
     }
-    if (outcomeToBool[branchOutcome] == true)
+
+    if (outcome_Bool[lookupTab] == true)
     {
-        uint32_t target = globalHistoryAndBranchTargetTable[addrTag].targetAddress;
+        uint32_t target = globalHist_BranchTargetTab[addrTag].Addres_Target;
         std::cout << "target:" << target << '\n';
         
         if(target)
@@ -168,69 +164,73 @@ uint32_t DynamicNTBranchPredictor::checkPredictorAndGetTarget(uint32_t addrTag)
     }
 }
 
-std::vector<int> DynamicNTBranchPredictor::getPatternHistory(uint32_t addrTag)
+std::vector<int> BranchPredict_NT::PatternHistory_fetch(uint32_t addrTag)
 {
-    return globalHistoryAndBranchTargetTable[addrTag].branchPatterHistory;
+    return globalHist_BranchTargetTab[addrTag].branchPatternHist;
 }
 
-int DynamicNTBranchPredictor::getBranchOutcome(std::vector<int> patternHistory)
+int BranchPredict_NT::Branch_Outcome(std::vector<int> patternHistory)
 {
     
-    return patternHistoryTable[patternHistory];
+    return patternHist_Tab[patternHistory];
 }
 
-void DynamicNTBranchPredictor::update(uint32_t PC, bool taken, uint32_t target, int opCode, int subopCode) {
+void BranchPredict_NT::update(uint32_t PC, bool take, uint32_t target, int op_Code, int subop_Code) {
     
-    if (not checkIfPCNotABranch(opCode, subopCode))
+    
+    if (not PCcheck_NotBranch(op_Code, subop_Code))
     {
     uint32_t addrTag = getTag(PC);
-    if (doesPCNotExistsInBTB_GHT(addrTag))
+    if (PCcheckInBTB_GHT(addrTag))
     {
-        addMissingPCInGlobalHistAndBranchTargetTable(addrTag, target, taken);
+        /* change BTB for the PC tag andPC target
+        // theen Replace Global history table with PC tag and initialize with "NNN"?!
+        // Check to update for branch,have to try
+        //negative 1 target for branch*/
+        addMissingPC_GlobalHistBranchTab(addrTag, target, take);
         
     }else{
-        std::vector<int> patternHistory = getPatternHistory(addrTag);
-        int branchOutcome = getBranchOutcome(patternHistory);
-        checkPredictionAndUpdate(branchOutcome, patternHistory, taken);
-        updateGlobalHistoryTable(addrTag, patternHistory, taken, target);
+        std::vector<int> patternHistory = PatternHistory_fetch(addrTag);
+        int lookupTab = Branch_Outcome(patternHistory);checkPrediction_Update(lookupTab, patternHistory, take);
+        GlobalHistTab_update(addrTag, patternHistory, take, target);
     }
-    mostRecentlyUsedAddrTag = addrTag;
+    AddrTag_mostRecent = addrTag;
     }
 }
 
-void DynamicNTBranchPredictor::addMissingPCInGlobalHistAndBranchTargetTable(uint32_t addrTag, uint32_t target, bool taken)
+void BranchPredict_NT::addMissingPC_GlobalHistBranchTab(uint32_t addrTag, uint32_t target, bool take)
 {
-    uint8_t addrTagToRemove = getAddrTagToRemove();
-    globalHistoryAndBranchTargetTable.erase(addrTagToRemove);
-    isBranchAndHistoryRow row;
-    row.targetAddress = target;
-    std::vector<int> previousBranchPattern (BHTWidth);
-    for (int j =0; j<BHTWidth-1; j++)
+    uint8_t addrTagToRemove = TagToRemove_getAddr();
+    globalHist_BranchTargetTab.erase(addrTagToRemove);
+    BranchHistory row;
+    row.Addres_Target = target;
+    std::vector<int> previousBranchPattern (Width_BHT);
+    for (int j =0; j<Width_BHT-1; j++)
     {
         previousBranchPattern.push_back(0);
     }
-    previousBranchPattern.push_back((taken)? takenCharacter : nottakenCharacter);
-    row.branchPatterHistory = previousBranchPattern;
-    globalHistoryAndBranchTargetTable[addrTag] = row;
+    previousBranchPattern.push_back((take)? take_Character : notTake_Character);
+    row.branchPatternHist = previousBranchPattern;
+    globalHist_BranchTargetTab[addrTag] = row;
 }
 
-uint32_t DynamicNTBranchPredictor::getAddrTagToRemove()
+uint32_t BranchPredict_NT::TagToRemove_getAddr()
 {
-    // Check if any existing free rows
-    std::map<uint32_t, isBranchAndHistoryRow>::iterator it;
-    for (it = globalHistoryAndBranchTargetTable.begin(); it != globalHistoryAndBranchTargetTable.end(); it++)
+    // to check for rows which are free
+    std::map<uint32_t, BranchHistory>::iterator it;
+    for (it = globalHist_BranchTargetTab.begin(); it != globalHist_BranchTargetTab.end(); it++)
     {
-        if (it->second.targetAddress == -1) {
+        if (it->second.Addres_Target == -1) {
             return it->first;
         }
     }
     
     while (true)
     {
-        auto it = globalHistoryAndBranchTargetTable.begin();
-        std::advance(it, rand() % globalHistoryAndBranchTargetTable.size());
+        auto it = globalHist_BranchTargetTab.begin();
+        std::advance(it, rand() % globalHist_BranchTargetTab.size());
         uint32_t random_key = it->first;
-        if(random_key!=mostRecentlyUsedAddrTag)
+        if(random_key!=AddrTag_mostRecent)
         {
             return random_key;
         }
@@ -238,48 +238,48 @@ uint32_t DynamicNTBranchPredictor::getAddrTagToRemove()
 }
 
 
-void DynamicNTBranchPredictor::updateGlobalHistoryTable(uint32_t addrTag, std::vector<int> patternHistory, bool taken, uint32_t target)
+void BranchPredict_NT::GlobalHistTab_update(uint32_t addrTag, std::vector<int> patternHistory, bool take, uint32_t target)
 {
-    isBranchAndHistoryRow row;
-    row.targetAddress = target;
+    BranchHistory row;
+    row.Addres_Target = target;
     std::vector<int> pattern = patternHistory;
     pattern.erase(pattern.begin());
-    pattern.push_back((taken)? takenCharacter : nottakenCharacter);
-    row.branchPatterHistory= pattern;
-    globalHistoryAndBranchTargetTable[addrTag] =row;
+    pattern.push_back((take)? take_Character : notTake_Character);
+    row.branchPatternHist= pattern;
+    globalHist_BranchTargetTab[addrTag] =row;
 }
 
-void DynamicNTBranchPredictor::checkPredictionAndUpdate(int branchOutcome, std::vector<int> patternHistory, bool taken)
+void BranchPredict_NT::checkPrediction_Update(int lookupTab, std::vector<int> patternHistory, bool take)
 {
-    if (outcomeToBool[branchOutcome] == taken)
+    if (outcome_Bool[lookupTab] == take)
     {
-        updatePHTForCorrectPrediction(branchOutcome, patternHistory);
+        PHTForCorrectPrediction_update(lookupTab, patternHistory);
         
     }else{ // Mis prediction
-        updatePHTForMisPrediction(branchOutcome, patternHistory);
+        PHTForMisPrediction_update(lookupTab, patternHistory);
     }
 }
 
-void DynamicNTBranchPredictor::updatePHTForCorrectPrediction(int branchOutcome, std::vector<int> patternHistory)
+void BranchPredict_NT::PHTForCorrectPrediction_update(int lookupTab, std::vector<int> patternHistory)
 {
-    // 2 scenarios
-    // 1. predicted not taken and not taken
-    if (outcomeToBool[branchOutcome] == false)
+    // there are only 2 possible sceneros
+    // correct take predict
+    if (outcome_Bool[lookupTab] == false) // Or take is false, reduce counter
     {
-        patternHistoryTable[patternHistory] = (branchOutcome-1<0) ? 0 : branchOutcome-1;
+        patternHist_Tab[patternHistory] = (lookupTab-1<0) ? 0 : lookupTab-1;
     }else{
-        // 2. predicted taken and taken
-        patternHistoryTable[patternHistory] = (branchOutcome+1> numberOfOutcomes-1)? numberOfOutcomes-1:branchOutcome+1;
+        // correct take predict
+        patternHist_Tab[patternHistory] = (lookupTab+1> OutcomeCount-1)? OutcomeCount-1:lookupTab+1;
     }
 }
 
-void DynamicNTBranchPredictor::updatePHTForMisPrediction(int branchOutcome, std::vector<int> patternHistory)
+void BranchPredict_NT::PHTForMisPrediction_update(int lookupTab, std::vector<int> patternHistory)
 {
-    // Predicted not taken but taken
-    if (outcomeToBool[branchOutcome] == false)
+    // False Not take predict
+    if (outcome_Bool[lookupTab] == false)
     {
-        patternHistoryTable[patternHistory] =  branchOutcome+1;
-    }else{ // Predicted taken but not taken
-        patternHistoryTable[patternHistory] =  branchOutcome-1;
+        patternHist_Tab[patternHistory] =  lookupTab+1;
+    }else{ // False take predict
+        patternHist_Tab[patternHistory] =  lookupTab-1;
     }
 }
